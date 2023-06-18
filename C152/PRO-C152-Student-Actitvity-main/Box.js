8a0c2c765d7e56975f8a5f302ec3251e2bdac710
yashwaerdhan <68593331+yashwaerdhan@users.noreply.github.com>
2023-06-18 14:56:30 +0530

    Add files via upload

---
+++ C152/PRO-C152-Student-Actitvity-main/Box.js
@@ -0,0 +1,20 @@
+// Registering component in box-component.js
+AFRAME.registerComponent("move-box-on-click", {
+  schema: {
+    moveX: { type: "number", default: 0 },
+  },
+
+  tick: function () {
+    window.addEventListener("click",(e)=>{
+      this.data.moveX = this.data.moveX + 0.009;
+    })
+
+    
+
+    var pos = this.el.getAttribute("position");
+
+    pos.x = pos.x+this.data.moveX;
+
+    this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z});
+  }
+});
---
+++ C152/PRO-C152-Student-Actitvity-main/README.md
@@ -0,0 +1,1 @@
+# PRO-C152-Student-Actitvity
